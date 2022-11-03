33e4ba6e052740c5267665dc6e4f6f375cffe8ea
Emmanuel Nwoye <emmanuelnwoye5@gmail.com>
2022-11-03 15:30:24 +0100

    feat (rules/favorites): -> added rules for favorites actions. feat (rules/viewHistory): added rules for viewHistory actions.

---
+++ app/rules/favorites.rules.js
@@ -0,0 +1,86 @@
+import { check } from 'express-validator';
+import db from "../models/index.js";
+import { default_status, default_delete_status } from '../config/config.js';
+
+const USERS = db.users;
+const FAVORITES = db.favorites;
+const PRODUCTS = db.products;
+
+export const favorite_rules = {
+    forFindingFavorite: [
+        check('user_unique_id', "User Unique Id is required")
+            .exists({ checkNull: true, checkFalsy: true })
+            .bail()
+            .custom(user_unique_id => {
+                return USERS.findOne({ where: { unique_id: user_unique_id, status: default_status } }).then(data => {
+                    if (!data) return Promise.reject('User not found!');
+                });
+            }),
+        check('unique_id', "Unique Id is required")
+            .exists({ checkNull: true, checkFalsy: true })
+            .bail()
+            .custom((unique_id, { req }) => {
+                return FAVORITES.findOne({
+                    where: {
+                        unique_id,
+                        user_unique_id: req.query.user_unique_id || req.body.user_unique_id || '',
+                        status: default_status
+                    }
+                }).then(data => {
+                    if (!data) return Promise.reject('Favorite not found!');
+                });
+            })
+    ],
+    forFindingFavoriteFalsy: [
+        check('user_unique_id', "User Unique Id is required")
+            .exists({ checkNull: true, checkFalsy: true })
+            .bail()
+            .custom(user_unique_id => {
+                return USERS.findOne({ where: { unique_id: user_unique_id, status: default_status } }).then(data => {
+                    if (!data) return Promise.reject('User not found!');
+                });
+            }),
+        check('unique_id', "Unique Id is required")
+            .exists({ checkNull: true, checkFalsy: true })
+            .bail()
+            .custom((unique_id, { req }) => {
+                return FAVORITES.findOne({
+                    where: {
+                        unique_id,
+                        user_unique_id: req.query.user_unique_id || req.body.user_unique_id || '',
+                        status: default_delete_status
+                    }
+                }).then(data => {
+                    if (!data) return Promise.reject('Favorite not found!');
+                });
+            })
+    ],
+    forFindingFavoriteAlt: [
+        check('favorite_unique_id', "Favorite Unique Id is required")
+            .exists({ checkNull: true, checkFalsy: true })
+            .bail()
+            .custom(favorite_unique_id => {
+                return FAVORITES.findOne({ where: { unique_id: favorite_unique_id, status: default_status } }).then(data => {
+                    if (!data) return Promise.reject('Favorite not found!');
+                });
+            })
+    ],
+    forAdding: [
+        check('user_unique_id', "User Unique Id is required")
+            .exists({ checkNull: true, checkFalsy: true })
+            .bail()
+            .custom(user_unique_id => {
+                return USERS.findOne({ where: { unique_id: user_unique_id, status: default_status } }).then(data => {
+                    if (!data) return Promise.reject('User not found!');
+                });
+            }),
+        check('product_unique_id', "Product Unique Id is required")
+            .exists({ checkNull: true, checkFalsy: true })
+            .bail()
+            .custom(product_unique_id => {
+                return PRODUCTS.findOne({ where: { unique_id: product_unique_id, status: default_status } }).then(data => {
+                    if (!data) return Promise.reject('Product not found!');
+                });
+            }),
+    ]
+};  
---
+++ app/rules/viewHistory.rules.js
@@ -0,0 +1,86 @@
+import { check } from 'express-validator';
+import db from "../models/index.js";
+import { default_status, default_delete_status } from '../config/config.js';
+
+const USERS = db.users;
+const VIEW_HISTORY = db.view_history;
+const PRODUCTS = db.products;
+
+export const view_history_rules = {
+    forFindingViewHistory: [
+        check('user_unique_id', "User Unique Id is required")
+            .exists({ checkNull: true, checkFalsy: true })
+            .bail()
+            .custom(user_unique_id => {
+                return USERS.findOne({ where: { unique_id: user_unique_id, status: default_status } }).then(data => {
+                    if (!data) return Promise.reject('User not found!');
+                });
+            }),
+        check('unique_id', "Unique Id is required")
+            .exists({ checkNull: true, checkFalsy: true })
+            .bail()
+            .custom((unique_id, { req }) => {
+                return VIEW_HISTORY.findOne({
+                    where: {
+                        unique_id,
+                        user_unique_id: req.query.user_unique_id || req.body.user_unique_id || '',
+                        status: default_status
+                    }
+                }).then(data => {
+                    if (!data) return Promise.reject('View History not found!');
+                });
+            })
+    ],
+    forFindingViewHistoryFalsy: [
+        check('user_unique_id', "User Unique Id is required")
+            .exists({ checkNull: true, checkFalsy: true })
+            .bail()
+            .custom(user_unique_id => {
+                return USERS.findOne({ where: { unique_id: user_unique_id, status: default_status } }).then(data => {
+                    if (!data) return Promise.reject('User not found!');
+                });
+            }),
+        check('unique_id', "Unique Id is required")
+            .exists({ checkNull: true, checkFalsy: true })
+            .bail()
+            .custom((unique_id, { req }) => {
+                return VIEW_HISTORY.findOne({
+                    where: {
+                        unique_id,
+                        user_unique_id: req.query.user_unique_id || req.body.user_unique_id || '',
+                        status: default_delete_status
+                    }
+                }).then(data => {
+                    if (!data) return Promise.reject('View History not found!');
+                });
+            })
+    ],
+    forFindingViewHistoryAlt: [
+        check('view_history_unique_id', "View History Unique Id is required")
+            .exists({ checkNull: true, checkFalsy: true })
+            .bail()
+            .custom(view_history_unique_id => {
+                return VIEW_HISTORY.findOne({ where: { unique_id: view_history_unique_id, status: default_status } }).then(data => {
+                    if (!data) return Promise.reject('View History not found!');
+                });
+            })
+    ],
+    forAdding: [
+        check('user_unique_id', "User Unique Id is required")
+            .exists({ checkNull: true, checkFalsy: true })
+            .bail()
+            .custom(user_unique_id => {
+                return USERS.findOne({ where: { unique_id: user_unique_id, status: default_status } }).then(data => {
+                    if (!data) return Promise.reject('User not found!');
+                });
+            }),
+        check('product_unique_id', "Product Unique Id is required")
+            .exists({ checkNull: true, checkFalsy: true })
+            .bail()
+            .custom(product_unique_id => {
+                return PRODUCTS.findOne({ where: { unique_id: product_unique_id, status: default_status } }).then(data => {
+                    if (!data) return Promise.reject('Product not found!');
+                });
+            }),
+    ]
+};  
