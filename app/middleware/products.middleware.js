22a44768da85fedf849e339e1dce3860bfb6dddd
Emmanuel Nwoye <emmanuelnwoye5@gmail.com>
2022-12-05 14:12:14 +0100

    refactor (middleware/*): added new image extension

--- app/middleware/products.middleware.js
+++ app/middleware/products.middleware.js
@@ -3,7 +3,7 @@ import { promisify } from "util";
 import { platform_documents_path } from "../config/config.js";
 
 const imageFilter = (req, file, cb) => {
-    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG)$/)) {
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|jfif|JFIF)$/)) {
         return cb(new Error('Only image files are allowed!'), false);
     } else {
         cb(null, true);
